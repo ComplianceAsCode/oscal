f810b844a4179e94991683d2f6989448108b2e9e
Šimon Lukašík <isimluk@fedoraproject.org>
2020-11-30 13:15:54 +0000

    Build instructions to produce JSON formatted OSCAL

--- .github/disregard-timestamp-changes.sh
+++ .github/disregard-timestamp-changes.sh
@@ -4,13 +4,18 @@ set -o errexit
 set -o nounset
 set -x
 
-cd xml/
-for f in `ls`; do
-	if ! git ls-files --error-unmatch $f; then
-		git add $f
-	fi
-	if ! git diff $f|grep -v last-modified.*last-modified | grep -v ' uuid="' | grep '^+\s'; then
-		git checkout $f;
-	fi
+for format in xml json; do
+	cd $format
+	for f in `ls`; do
+		if ! git ls-files --error-unmatch $f; then
+			git add $f
+		fi
+		if [ "x$format" == "xxml"  ] && ! git diff $f|grep -v last-modified.*last-modified | grep -v ' uuid="' | grep '^+\s'; then
+			git checkout $f;
+		fi
+		if [ "x$format" == "xjson" ] && ! git diff $f|grep -v '"last-modified": ".*",$'  | grep -v '"uuid": "' | grep '^+\s'; then
+			git checkout $f;
+		fi
+	done
+	cd ..
 done
-cd ..
--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
-oscal: xml
+oscal: xml json
 all: oscal docx
 
 
 xml: gocomply_fedramp
-	gocomply_fedramp --debug opencontrol https://github.com/ComplianceAsCode/redhat xml/
+	gocomply_fedramp --debug opencontrol --format xml https://github.com/ComplianceAsCode/redhat xml/
+
+json: gocomply_fedramp
+	gocomply_fedramp --debug opencontrol --format json https://github.com/ComplianceAsCode/redhat json/
 
 docx: gocomply_fedramp $(shell ls -d xml/* | sed -e 's/xml/docx/g')
 
@@ -15,4 +18,4 @@ ifeq ("$(wildcard $(GOPATH)/bin/gocomply_fedramp)","")
 	go get -u -v github.com/gocomply/fedramp/cli/gocomply_fedramp
 endif
 
-.PHONY: gocomply_fedramp oscal xml
+.PHONY: gocomply_fedramp oscal xml json
